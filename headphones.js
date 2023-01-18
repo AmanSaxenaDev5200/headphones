3fc34b207e4f74e2a71cb64c90f68f2c8f911ad0
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-01-18 15:49:56 -0800

    Add functionality to open/close login modal

--- headphones.js
+++ headphones.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+'use strict';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+//login modal
+const signup = document.getElementById('signup');
+const closebtn = document.getElementById('close-modal');
+signup.addEventListener('click', openModal, false);
+closebtn.addEventListener('click', closeModal, false);
+
+
+function openModal() {
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+    if (modal.classList.contains('hidden')) {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    }
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+});
