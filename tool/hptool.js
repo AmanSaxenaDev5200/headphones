e22cf556f3a883b45c1146b579e2d6d9fbedc403
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-01-19 17:57:38 -0800

    Add adjustment to test tool for CORS issue

--- tool/hptool.js
+++ tool/hptool.js
@@ -1,7 +1,7 @@
 const headphones = [];
 console.log('yo');
 async function loadHeadphones() {
-    const response = await fetch('./headphones.json');
+    const response = await fetch('https://raw.githubusercontent.com/yd24/cf-project/main/tool/headphones.json');
     const names = await response.json();
     console.log(names);
 }
